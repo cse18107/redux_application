bb2d2ecd0cbabf64a1d543d2f6cfa427e99fee57
cse18107 <cse18107@cemk.ac.in>
2021-12-20 00:27:35 +0530

    adding payload to our redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,31 @@
 import { createStore } from 'redux';
 
-const counterReducer =  (state = {counter: 0}, action ) => {
+
+const initialState={counter: 0,showCounter:true}
+
+const counterReducer =  (state = initialState, action ) => {
     if(action.type === 'increment'){
         return {
-            counter :state.counter+1
+            counter :state.counter+1,
+            showCounter:true
         }
     }
     if(action.type === 'decrement'){
         return {
-            counter : state.counter-1
+            counter : state.counter-1,
+            showCounter:true
         }
     }
     if(action.type === 'increase'){
         return {
-            counter :state.counter+action.amount
+            counter :state.counter+action.amount,
+            showCounter:true
         }
     }
     if(action.type==='toggle'){
         return {
-            counter:0
+            showCounter:!state.showCounter,
+            counter:state.counter
         }
     }
 
