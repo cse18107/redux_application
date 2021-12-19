5167e0b6cc21524b27ea4a230a79c7765cce412b
cse18107 <cse18107@cemk.ac.in>
2021-12-19 20:21:39 +0530

    connecting redux store to our component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,32 @@
 import classes from './Counter.module.css';
+import { useSelector,useDispatch } from 'react-redux';
+
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state=>state.counter);
+
+  const incrementHandler = () =>{
+    dispatch({type:"increment"})
+  }
+
+  
+  const decrementHandler = () =>{
+    dispatch({type:"decrement"});
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
+
+    
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>INCREMENT</button>
+        <button onClick={decrementHandler}>DECREMENT</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
